3aebb368c282938a3d6f74f5b9426f4f8185ca8c
UMUT BAYRAM <umut.bayram@getir.com>
2022-06-24 16:35:07 +0300

    init project

---
+++ src/main/java/com/jobrunrsample/JobRunrSampleApplication.java
@@ -0,0 +1,13 @@
+package com.jobrunrsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobRunrSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobRunrSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jobrunrsample/JobRunrSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobrunrsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobRunrSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
